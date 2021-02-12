9f93aed903fa70653085951ef283a52693dae1b8
afraefatuaki <afrae@elfatuaki.com>
2021-02-12 15:20:23 +0100

    add change-text

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,20 +5,26 @@ let sevenPercent = document.getElementById('seven')
 let price = document.getElementById('price')
 let texas = document.getElementById('texas')
 let total = document.getElementById('total')
+let Label1 = document.getElementById('brutoLabel')
+let Label2 = document.getElementById('netoLabel')
 
 
 function texasCalculation() {
-    if (nettoZuBrutto.checked == true && nineteenPercent.checked == true) {
+    if (nettoZuBrutto.checked && nineteenPercent.checked) {
         texas.innerHTML = (price.value * 0.19).toFixed(2)
         total.innerHTML = (price.value * 1.19).toFixed(2)
-    } else if (nettoZuBrutto.checked == true && sevenPercent.checked == true) {
+    } else if (nettoZuBrutto.checked && sevenPercent.checked) {
         texas.innerHTML = (price.value * 0.07).toFixed(2)
         total.innerHTML = (price.value * 1.07).toFixed(2)
-    } else if (bruttoZuNetto.checked == true && nineteenPercent.checked == true) {
+    } else if (bruttoZuNetto.checked && nineteenPercent.checked) {
         texas.innerHTML = (price.value / 1.19 * 0.19).toFixed(2)
         total.innerHTML = (price.value / 1.19).toFixed(2)
-    } else if (bruttoZuNetto.checked == true && sevenPercent.checked == true) {
+    } else if (bruttoZuNetto.checked && sevenPercent.checked) {
         texas.innerHTML = (price.value / 1.07 * 0.07).toFixed(2)
         total.innerHTML = (price.value / 1.07).toFixed(2)
     }
+}
+
+function changeLabel() {
+    bruttoZuNetto.checked ? (Label1.innerHTML = 'Bruttobetrag') && (Label2.innerHTML = 'Nettobetrag') : (Label1.innerHTML = 'Nettobetrag') && (Label2.innerHTML = 'Bruttobetrag')
 }
